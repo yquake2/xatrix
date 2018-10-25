a0fbb36fbc9d42ab53402d230776d485f7b7d121
Yamagi Burmeister <yamagi@yamagi.org>
2018-10-25 19:15:41 +0200

    Import Q_strlcpy() and replace several dangerous strncpy() with it.

--- src/header/shared.h
+++ src/header/shared.h
@@ -237,6 +237,9 @@ extern int curtime; /* time returned by last Sys_Milliseconds */
 int Sys_Milliseconds(void);
 void Sys_Mkdir(char *path);
 char *strlwr(char *s);
+/* portable safe string copy/concatenate */
+int Q_strlcpy(char *dst, const char *src, int size);
+int Q_strlcat(char *dst, const char *src, int size);
 
 /* large block stack allocation routines */
 void *Hunk_Begin(int maxsize);
--- src/shared/shared.c
+++ src/shared/shared.c
@@ -1098,6 +1098,42 @@ strlwr ( char *s )
 	return ( p );
 }
 
+int
+Q_strlcpy(char *dst, const char *src, int size)
+{
+	const char *s = src;
+
+	while (*s)
+	{
+		if (size > 1)
+		{
+			*dst++ = *s;
+			size--;
+		}
+		s++;
+	}
+	if (size > 0)
+	{
+		*dst = '\0';
+	}
+
+	return s - src;
+}
+
+int
+Q_strlcat(char *dst, const char *src, int size)
+{
+	char *d = dst;
+
+	while (size > 0 && *d)
+	{
+		size--;
+		d++;
+	}
+
+	return (d - dst) + Q_strlcpy(d, src, size);
+}
+
 /*
  * =====================================================================
  *
