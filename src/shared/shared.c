0dc3c87742ccbe2c5e26c65d524dbfc65ff75d1b
David CARLIER <devnexen@gmail.com>
2022-10-23 22:27:23 +0100

    va little signature change

--- src/shared/shared.c
+++ src/shared/shared.c
@@ -884,7 +884,7 @@ Swap_Init(void)
  * need to have varargs versions of all text functions.
  */
 char *
-va(char *format, ...)
+va(const char *format, ...)
 {
 	va_list argptr;
 	static char string[1024];
