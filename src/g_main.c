a43de2201316b4a130d1ff55142deda8c6c7020a
Jaime Moreira <jmoreirag@gmail.com>
2023-06-28 15:57:43 -0400

    Cheat cvar "g_swap_speed" implemented
    Allows to skip frames of "putting down weapon" and
    "raising weapon" animations, speeding them up.

--- src/g_main.c
+++ src/g_main.c
@@ -68,6 +68,7 @@ cvar_t *gib_on;
 
 cvar_t *aimfix;
 cvar_t *g_machinegun_norecoil;
+cvar_t *g_swap_speed;
 
 void SpawnEntities(char *mapname, char *entities, char *spawnpoint);
 void ClientThink(edict_t *ent, usercmd_t *cmd);
